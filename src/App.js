aaf3c5f97c36bd6b1c9e43e4a8718c468879c246
kyosuke <kn1359028527@gmail.com>
2021-11-07 13:41:21 +0900

    learn props of componet

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import React from "react";
 
-
-// function App() {
-
-//   return(
-//     <React.Fragment>
-//       <label htmlFor="bar">bar</label>
-//       <input type="text" onChange={()=>{console.log("I am clicked!")}} />
-//     </React.Fragment>
-//   )
-// }
-
 const App=()=>{
+  const profile=[
+    {name:"Taro",age:10},
+    {name:"Hanako",age:5},
+    {name:"noname"}
+  ]
     return (
       <div>
-        <Cat />
-        <Cat />
-        <Cat />
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={()=>{console.log("I am clicked!")}} />
+        {
+          profile.map((profile,index)=>{
+            return <User name={profile.name} age={profile.age} key={index}/>//関数の中で値を返却している
+          })
+        }
     </div>
   )
 }
-const Cat =()=>{
-  return  <div>Meow!</div>
+
+const User =(props)=>{
+  return  <div>Hi, I am {props.name},and {props.age} years old!</div>
+}
+
+User.defaultProps={
+  age:1
 }
 
 export default App;
